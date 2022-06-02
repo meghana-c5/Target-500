60de7ce927bb7e65020d4813f46cfeccb29dc930
yerramsettysaimeghana <saimeghanayerramsetty@gmail.com>
2022-06-02 08:39:00 +0530

    change file

--- Sorting/01_bubble_sort.cpp
+++ Sorting/01_bubble_sort.cpp
@@ -9,7 +9,7 @@ int main()
  
     int arr[] = {6,3,9,5,2,8,7};
 
-    // swap adjacent elements 
+    // swap adjacent elements repeatedly
     int n = sizeof(arr)/sizeof(arr[0]);
     for(int  i = 0; i < n; i++){
         for(int j = 0; j < n - i; j++){
