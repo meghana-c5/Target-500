5eca33ea2c949e92c9d2b8765bfa93af52003c41
yerramsettysaimeghana <saimeghanayerramsetty@gmail.com>
2022-06-02 08:25:50 +0530

    change file

---
+++ Sorting/01_bubble_sort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+#define fo(a,b,c) for(int a = b; a < c ; a++)
+ 
+int main() 
+{
+ 
+    int arr[] = {6,3,9,5,2,8,7};
+
+    // swap adjacent elements 
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int  i = 0; i < n; i++){
+        for(int j = 0; j < n - i; j++){
+            if(arr[j] > arr[j + 1])
+                swap(arr[j], arr[j+1]);
+        }
+    }
+
+    fo(i,0,n)
+        cout<<arr[i]<<" ";
+ 
+return 0;
+   
+}
